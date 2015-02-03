ecdef582e616534b9894291f872aae6b6a4b0b28
Bruno Philipe <brunophilipe@me.com>
2015-02-03 00:06:12 -0200

    Added Sun, Saturn, Jupiter. Now draws orbits.

--- assets/js/planetarium.js
+++ assets/js/planetarium.js
@@ -9,25 +9,60 @@
  * http://www.met.rdg.ac.uk/~ross/Astronomy/Planets.html (positioning data)
  * http://astro.unl.edu/naap/pos/animations/kepler.html (reference)
  */
-var Planetarium = (function() {
+var Planetarium = (function(camera) {
     var module = {};
 
     Date.prototype.getJulian = function() {
         return Math.floor((this / 86400000) - (this.getTimezoneOffset()/1440) + 2440587.5);
-    }
+    };
 
     module.daySpan = 0;
+    module.AU = 149597.871 / 20;
 
     module.bodies = [
         {
+            name: "Sun",
+            mainMesh: {
+                geometry: new THREE.SphereGeometry(686.342, 32, 32),
+                material: new THREE.MeshPhongMaterial({
+                    map: THREE.ImageUtils.loadTexture("assets/img/sun/map1k.jpg"),
+                    shininess: 500,
+                    combine: THREE.MixOperation
+                })
+            },
+            subMeshes: [
+                {
+                    geometry: new THREE.SphereGeometry(697.342, 32, 32),
+                    material: new THREE.ShaderMaterial({
+                        uniforms:
+                        {
+                            "c":   { type: "f", value: 0.0 },
+                            "p":   { type: "f", value: 0.0 },
+                            glowColor: { type: "c", value: new THREE.Color(0xffff00) },
+                            viewVector: { type: "v3", value: camera.position }
+                        },
+                        vertexShader:   document.getElementById( 'vertexShader'   ).textContent,
+                        fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+                        side: THREE.FrontSide,
+                        blending: THREE.MultiplyBlending,
+                        transparent: true,
+                        opacity: 0.5
+                    })
+                }
+            ],
+            orbitalElements: null,
+            rotationPeriod: 1/25.05
+        },
+        {
             name: "Mercury",
             mainMesh: {
-                scale: 2.4397,
-                material: {
+                geometry: new THREE.SphereGeometry(2.4397, 32, 32),
+                material: new THREE.MeshPhongMaterial({
                     map: THREE.ImageUtils.loadTexture("assets/img/mercury/map1k.jpg"),
                     bumpMap: THREE.ImageUtils.loadTexture("assets/img/mercury/bump1k.jpg"),
-                    bumpScale: 0.01
-                }
+                    bumpScale: 0.01,
+                    shininess: 5
+                })
             },
             subMeshes: [],
             orbitalElements: {
@@ -36,29 +71,31 @@ var Planetarium = (function() {
                 i: 7.00500,     //inclination
                 O: 48.3310,     //Omega
                 o: 29.1250,     //~omega
-                M: 174.795,     //Mean anomaly
+                M: 174.795,     //Mean anomaly,
+                p: 0.240846,    //Sideral Orbital Period
                 refDate: 2451545
-            }
+            },
+            rotationPeriod: 1/58.646
         },{
             name: "Venus",
             mainMesh: {
-                scale: 6.0518,
-                material: {
+                geometry: new THREE.SphereGeometry(5.7518, 32, 32),
+                material: new THREE.MeshPhongMaterial({
                     map: THREE.ImageUtils.loadTexture("assets/img/venus/map1k.jpg"),
                     bumpMap: THREE.ImageUtils.loadTexture("assets/img/venus/bump1k.jpg"),
-                    bumpScale: 0.01
-                }
+                    bumpScale: 0.01,
+                    shininess: 10
+                })
             },
             subMeshes: [
                 {
-                    scale: 6.3018,
-                    material: {
+                    geometry: new THREE.SphereGeometry(6.2018, 32, 32),
+                    material: new THREE.MeshPhongMaterial({
                         map     : THREE.ImageUtils.loadTexture("assets/img/venus/cloudMap.jpg"),
                         side        : THREE.DoubleSide,
                         opacity     : 0.85,
-                        transparent : true,
-                        depthWrite  : true
-                    }
+                        transparent : true
+                    })
                 }
             ],
             orbitalElements: {
@@ -67,32 +104,35 @@ var Planetarium = (function() {
                 i: 3.39500,     //inclination
                 O: 76.6800,     //Omega
                 o: 54.8840,     //~omega
-                M: 50.4160,     //Mean anomaly
+                M: 50.4160,     //Mean anomaly,
+                p: 0.615,    //Sideral Orbital Period
                 refDate: 2451545
-            }
+            },
+            rotationPeriod: -1/243.0185
         },
         {
             name: "Earth",
             mainMesh: {
-                scale: 6.371,
-                material: {
+                geometry: new THREE.SphereGeometry(6.371, 32, 32),
+                material: new THREE.MeshPhongMaterial({
                     map: THREE.ImageUtils.loadTexture("assets/img/earth/map1k.jpg"),
                     bumpMap: THREE.ImageUtils.loadTexture("assets/img/earth/bump1k.jpg"),
                     bumpScale: 0.01,
                     specularMap: THREE.ImageUtils.loadTexture("assets/img/earth/spec1k.jpg"),
-                    specular: new THREE.Color('grey')
-                }
+                    specular: new THREE.Color('grey'),
+                    shininess: 5
+                })
             },
             subMeshes: [
                 {
-                    scale: 6.471,
-                    material: {
+                    geometry: new THREE.SphereGeometry(6.771, 32, 32),
+                    material: new THREE.MeshPhongMaterial({
                         map     : THREE.ImageUtils.loadTexture("assets/img/earth/cloudMap.png"),
                         side        : THREE.DoubleSide,
                         opacity     : 0.8,
                         transparent : true,
-                        depthWrite  : false
-                    }
+                        depthWrite  : true
+                    })
                 }
             ],
             orbitalElements: {
@@ -101,21 +141,24 @@ var Planetarium = (function() {
                 i: 0.00005,     //inclination
                 O: 174.873,     //Omega
                 o: 288.064,     //~omega
-                M: 357.529,     //Mean anomaly
+                M: 357.529,     //Mean anomaly,
+                p: 1.0,    //Sideral Orbital Period
                 refDate: 2451545
-            }
+            },
+            rotationPeriod: 1
         },
         {
             name: "Mars",
             mainMesh: {
-                scale: 3.3895,
-                material: {
+                geometry: new THREE.SphereGeometry(3.3895, 32, 32),
+                material: new THREE.MeshPhongMaterial({
                     map: THREE.ImageUtils.loadTexture("assets/img/mars/map1k.jpg"),
                     bumpMap: THREE.ImageUtils.loadTexture("assets/img/mars/bump1k.jpg"),
                     bumpScale: 0.01,
                     specularMap: THREE.ImageUtils.loadTexture("assets/img/mars/spec1k.jpg"),
-                    specular: new THREE.Color('grey')
-                }
+                    specular: new THREE.Color('grey'),
+                    shininess: 5
+                })
             },
             subMeshes: [],
             orbitalElements: {
@@ -125,37 +168,103 @@ var Planetarium = (function() {
                 O: 49.5580,     //Omega
                 o: 286.502,     //~omega
                 M: 19.3730,     //Mean anomaly
+                p: 1.881,    //Sideral Orbital Period
                 refDate: 2451545
-            }
+            },
+            rotationPeriod: 1/1.025957
+        },
+        {
+            name: "Jupiter",
+            mainMesh: {
+                geometry: new THREE.SphereGeometry(69.9110, 32, 32),
+                material: new THREE.MeshPhongMaterial({
+                    map: THREE.ImageUtils.loadTexture("assets/img/jupiter/map2k.jpg"),
+                    shininess: 5
+                })
+            },
+            subMeshes: [],
+            orbitalElements: {
+                a: 5.20260,     //mean distance
+                e: 0.04849,     //eccentricity
+                i: 1.30300,     //inclination
+                O: 100.464,     //Omega
+                o: 273.867,     //~omega
+                M: 20.0200,     //Mean anomaly,
+                p: 11.86,    //Sideral Orbital Period
+                refDate: 2451545
+            },
+            rotationPeriod: 1/0.41
+        },
+        {
+            name: "Saturn",
+            mainMesh: {
+                geometry: new THREE.SphereGeometry(60.268, 32, 32),
+                material: new THREE.MeshPhongMaterial({
+                    map: THREE.ImageUtils.loadTexture("assets/img/saturn/map1k.jpg"),
+                    shininess: 5
+                })
+            },
+            subMeshes: [],
+            orbitalElements: {
+                a: 9.55491,     //mean distance
+                e: 0.05551,     //eccentricity
+                i: 2.48900,     //inclination
+                O: 113.666,     //Omega
+                o: 339.391,     //~omega
+                M: 317.021,     //Mean anomaly
+                p: 29.46,       //Sideral Orbital Period
+                refDate: 2451545
+            },
+            rotationPeriod: 1/0.44
         }
     ];
 
-    module.positionForBodyWithID = function(id)
+    module.positionForBodyWithID = function(id, span)
     {
         var body = this.bodies[id];
         var ob = body.orbitalElements;
-        var today = new Date();
-        var d2r = Math.PI/180.0;
 
-        //mean anomaly
-        var n = 0.9856076686/Math.pow(ob.a, 3.0/2.0);
-        var M = ob.M + n * (today.getJulian() - ob.refDate + this.daySpan);
+        if (ob !== null)
+        {
+            var today = new Date();
+            var d2r = Math.PI/180.0;
 
-        //true anomaly (nu)
-        var nu = trueAnomaly(ob.e, M, 10);
+            //mean anomaly
+            var n = 0.9856076686/Math.pow(ob.a, 3.0/2.0);
+            var M = ob.M + n * (today.getJulian() - ob.refDate + span);
 
-        //distance to sun
-        var A = (ob.a * (1 - Math.pow(ob.e, 2)));
-        var B = ob.e * Math.cos(nu * d2r);
-        var r = A/(1 + B);
+            //true anomaly (nu)
+            var nu = trueAnomaly(ob.e, M, 10);
 
-        //three-dimensional astronomical position relative to the sun
-        var PosX = r * (Math.cos(ob.O * d2r) * Math.cos((ob.o + nu) * d2r) - Math.sin(ob.O * d2r) * Math.cos(ob.i * d2r) * Math.sin((ob.o + nu) * d2r));
-        var PosY = r * (Math.sin(ob.O * d2r) * Math.cos((ob.o + nu) * d2r) + Math.cos(ob.O * d2r) * Math.cos(ob.i * d2r) * Math.sin((ob.o + nu) * d2r));
-        var PosZ = r * Math.sin(ob.i * d2r) * Math.sin((ob.o + nu) * d2r);
+            //distance to sun
+            var r = (ob.a * (1 - Math.pow(ob.e, 2)))/(1 + (ob.e * Math.cos(nu * d2r)));
 
-        return {x: PosX, y: PosY, z: PosZ};
-    }
+            //three-dimensional astronomical position relative to the sun
+            var PosX = r * (Math.cos(ob.O * d2r) * Math.cos((ob.o + nu) * d2r) - Math.sin(ob.O * d2r) * Math.cos(ob.i * d2r) * Math.sin((ob.o + nu) * d2r));
+            var PosY = r * (Math.sin(ob.O * d2r) * Math.cos((ob.o + nu) * d2r) + Math.cos(ob.O * d2r) * Math.cos(ob.i * d2r) * Math.sin((ob.o + nu) * d2r));
+            var PosZ = r * Math.sin(ob.i * d2r) * Math.sin((ob.o + nu) * d2r);
+
+            return {x: PosX, y: PosY, z: PosZ};
+        }
+        else
+        {
+            return {x: 0.0, y: 0.0, z: 0.0};
+        }
+    };
+
+    module.pointsForOrbitOfBodyWithID = function(bodyID)
+    {
+        var body = this.bodies[bodyID];
+        var pointCount = Math.floor(body.orbitalElements.p * 365);
+        var points = [];
+
+        for (var d=0; d<pointCount; d++)
+        {
+            points.push(this.positionForBodyWithID(bodyID, d));
+        }
+
+        return points;
+    };
 
     function trueAnomaly(ec,m,dp) {
         var pi = Math.PI;
--- assets/js/scene.js
+++ assets/js/scene.js
@@ -4,77 +4,141 @@
 var Scene = (function(){
     var module = {};
 
+    var canvas;
     var scene, camera, sunLight, ambientLight, renderer;
     var planetarium;
+    var origin;
     var meshes_bodies = [];
+    var meshes_orbitPaths = [];
+
+    var width, height;
+
+    module.enableLabels = false;
 
     module.initScene = function() {
         scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-        camera.position.set(700,0,500);
+
+        width = window.innerWidth;
+        height = window.innerHeight;
+        origin = new THREE.Vector3(0,0,0);
+
+//      camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 0.1, 100000);
+//      camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 100000);
+        camera = new THREE.CombinedCamera(width, height, 60, 1, 200000, -200000, 200000);
+
+//        camera.position.set(3000,-10000,900);
+        camera.position.set(0,0,50000);
         camera.up = new THREE.Vector3(0,0,1);
-        camera.lookAt(new THREE.Vector3(0,0,0));
+        camera.lookAt(origin);
 
         sunLight = new THREE.PointLight(0xFFFFFF);
         sunLight.position.set(0, 0, 0);
         scene.add(sunLight);
 
-        ambientLight = new THREE.AmbientLight(0x303030);
+        ambientLight = new THREE.AmbientLight(0xAAAAAA);
         scene.add(ambientLight);
 
-        renderer = new THREE.WebGLRenderer({ antialiasing: true });
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+        renderer = new THREE.WebGLRenderer({
+            antialiasing: true
+        });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(width, height);
 
-        camera.updateProjectionMatrix();
+        canvas = renderer.domElement;
+//        alert(canvas.cssText );
+        canvas.style.cssText += "position: absolute; top:0; left:0;";
 
-        planetarium = new Planetarium();
+        document.body.appendChild(canvas);
+
+//        camera.toOrthographic();
+
+        planetarium = new Planetarium(camera);
+
+        configureCanvas(canvas, width, height);
 
         this.load();
         render();
     };
 
     module.load = function() {
-        var geometry = new THREE.SphereGeometry(5, 32, 32);
-        var material = new THREE.MeshNormalMaterial();
-        var bodyMesh = new THREE.Mesh(geometry, material);
-        scene.add(bodyMesh);
+        var d2r = Math.PI/180.0;
 
-        geometry  = new THREE.SphereGeometry(1500, 64, 64)
-        material  = new THREE.MeshBasicMaterial()
-        material.map   = THREE.ImageUtils.loadTexture('assets/img/galaxy_starfield.png');
-        material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
-        material.map.repeat.set(8,5);
-        material.side  = THREE.BackSide;
-
-        bodyMesh  = new THREE.Mesh(geometry, material)
-        scene.add(bodyMesh);
+        var bgGeometry  = new THREE.SphereGeometry(100000, 64, 64);
+        var bgMaterial  = new THREE.MeshBasicMaterial();
+        bgMaterial.map   = THREE.ImageUtils.loadTexture('assets/img/galaxy_starfield.png');
+        bgMaterial.map.wrapS = bgMaterial.map.wrapT = THREE.RepeatWrapping;
+        bgMaterial.map.repeat.set(8,5);
+        bgMaterial.side  = THREE.BackSide;
+        var bgMesh  = new THREE.Mesh(bgGeometry, bgMaterial);
+        scene.add(bgMesh);
 
         for (var bodyID=0; bodyID<planetarium.bodies.length; ++bodyID)
         {
             var body = planetarium.bodies[bodyID];
 
-            geometry = new THREE.SphereGeometry(body.mainMesh.scale, 32, 32);
-            material = new THREE.MeshPhongMaterial(body.mainMesh.material);
-            bodyMesh = new THREE.Mesh(geometry, material);
+            var geometry = body.mainMesh.geometry;
+            var material = body.mainMesh.material;
+            var bodyMesh = new THREE.Mesh(geometry, material);
             bodyMesh.bodyID = bodyID;
+            bodyMesh.rotationPeriod = body.rotationPeriod;
 
             for (var subMeshID=0; subMeshID<body.subMeshes.length; ++subMeshID)
             {
                 var subMeshData = body.subMeshes[subMeshID];
-                geometry = new THREE.SphereGeometry(subMeshData.scale, 32, 32);
-                material = new THREE.MeshPhongMaterial(subMeshData.material);
-                var subMesh = new THREE.Mesh(geometry, material);
+                var subMesh = new THREE.Mesh(subMeshData.geometry, subMeshData.material);
+
                 bodyMesh.add(subMesh);
             }
 
-            var pos = planetarium.positionForBodyWithID(bodyID);
+            if (module.enableLabels)
+            {
+                var labelGeometry = new THREE.PlaneGeometry(1,0.2);
+                var labelMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+                var labelMesh = new THREE.Mesh(labelGeometry, labelMaterial);
+                labelMesh.position.set(100, 100, 100);
+                bodyMesh.add(subMesh);
+            }
+
+            if (bodyID > 0)
+            {
+                bodyMesh.scale.set(5, 5, 5);
+
+                var orbitMaterial = new THREE.LineBasicMaterial({
+                    color: 0x0000ff,
+                    lineWidth: 5
+                });
+                var orbitPoints = planetarium.pointsForOrbitOfBodyWithID(bodyID);
+                var orbitGeometry = new THREE.Geometry();
+
+                function addPoint(point, geometry, skipAU)
+                {
+                    if (!skipAU)
+                    {
+                        point.x *= planetarium.AU;
+                        point.y *= planetarium.AU;
+                        point.z *= planetarium.AU;
+                    }
+                    geometry.vertices.push(point);
+                }
+
+                for (var p=0; p<orbitPoints.length; p++)
+                {
+                    addPoint(orbitPoints[p], orbitGeometry, false);
+                }
+                addPoint(orbitPoints[0], orbitGeometry, true);
+
+                var orbitMesh = new THREE.Line(orbitGeometry, orbitMaterial, THREE.LineStrip);
+                scene.add(orbitMesh);
+            }
+
+            var pos = planetarium.positionForBodyWithID(bodyID, planetarium.daySpan);
 //            console.log(pos.x * 100 + ", " + pos.y * 100 + ", " + pos.z * 100);
 
             scene.add(bodyMesh);
             meshes_bodies.push(bodyMesh);
 
-            bodyMesh.position.set(pos.x * 400, pos.y * 400, pos.z * 400);
+            bodyMesh.position.set(pos.x * planetarium.AU, pos.y * planetarium.AU, pos.z * planetarium.AU);
+            bodyMesh.rotateOnAxis(new THREE.Vector3(1.0,0.0,0),90 * d2r);
         }
     };
 
@@ -83,22 +147,43 @@ var Scene = (function(){
     var render = function() {
         requestAnimationFrame(render, null);
 
-        count++;
-        if (count==2)
+        const daysPassed = 0.25;
+
+        planetarium.daySpan+=daysPassed;
+
+        if (count < 2240)
         {
-            count = 0;
-            planetarium.daySpan++;
+            camera.position.set(count < 100 ? count * 15 : count * 15 - count * 10,0,50000 - count * 20);
+            camera.lookAt(origin);
+
+            count++;
         }
 
         for (var bodyID=0; bodyID<meshes_bodies.length; ++bodyID)
         {
             var body = meshes_bodies[bodyID];
-            var pos = planetarium.positionForBodyWithID(body.bodyID);
-            body.position.set(pos.x * 200, pos.y * 200, pos.z * 200);
+//            if (bodyID==3)
+//            {
+//                camera.lookAt(body.position);
+//            }
+            var pos = planetarium.positionForBodyWithID(body.bodyID, planetarium.daySpan);
+            body.position.set(pos.x * planetarium.AU, pos.y * planetarium.AU, pos.z * planetarium.AU);
+            body.rotation.y += body.rotationPeriod * daysPassed;
         }
 
         renderer.render(scene, camera);
     };
+
+    function configureCanvas(canvas, width, height)
+    {
+//        var devicePixelRatio = window.devicePixelRatio || 1;
+//
+//        canvas.style.width = width + "px";
+//        canvas.style.height = height + "px";
+//
+//        canvas.width = width * devicePixelRatio;
+//        canvas.height = height * devicePixelRatio;
+    }
 
     return module;
 });
